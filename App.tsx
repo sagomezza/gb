6a7570b77b2f0fa826e4dc5c7eac3c752fd82bf2
Gerardo Cordero <gcordero@novacompcr.com>
2021-05-18 21:52:19 +0000

    Merged in feature/APK-235-SplashScreenUIOnly (pull request #4)
    [APK-235] Android/iOS Splash and assets
    
    * Android/iOS Splash and assets
    
    * Android/iOS Splash and assets
    
    * Merge branch 'feature/APK-235-SplashScreenUIOnly'
    
    * Fix
    
    * Info.plist
    * Merged develop into feature/APK-235-SplashScreenUIOnly
    
    Approved-by: Javier Olivieri

--- App.tsx
+++ App.tsx
@@ -1,25 +1,37 @@
 // @ts-nocheck
-import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Provider as StoreProvider } from 'react-redux';
-import { store } from 'store';
-import RNBootSplash from 'react-native-bootsplash';
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
+import { store } from "store";
+import RNBootSplash from "react-native-bootsplash";
 
 // UI
-import { theme as themePaper } from './src/config/theme';
-import AppNavigator from './src/navigation/AppNavigator';
+import { theme as themePaper } from "./src/config/theme";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 // Uncomment line 16 when doing tests
 // LogBox.ignoreAllLogs(true);
 
 // RNBootSplash.hide({ fade: true });
 
-const App = () => (
-  <StoreProvider store={store}>
-    <PaperProvider theme={themePaper}>
-      <AppNavigator />
-    </PaperProvider>
-  </StoreProvider>
-);
+const App = () => {
+  React.useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true });
+      console.log("Bootsplash has been hidden successfully");
+    });
+  }, []);
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider theme={themePaper}>
+        <AppNavigator />
+      </PaperProvider>
+    </StoreProvider>
+  );
+};
 
 export default App;
