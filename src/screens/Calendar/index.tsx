5757d48ab5bc8942dd589d822c71f9ac047a2334
Santiago <sagomezza@Santiagos-MacBook-Pro.local>
2021-10-25 05:31:22 -0500

    BE integration

--- src/screens/Calendar/index.tsx
+++ src/screens/Calendar/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { Spacing } from 'core/components';
-import { DefaultIcon, GBScreenHeader } from 'components';
+import { ActivityIndicator, DefaultIcon, GBScreenHeader, SafeArea } from 'components';
 import { markedDates } from 'utils/calendar-data-mock';
 import routes from 'config/routes';
-import { SafeAreaView } from 'screens/styles';
+import { Activity } from 'lib/api';
+import { listActivitiesQuery } from 'service/queries';
+import { getUserId } from 'store/auth/authSelectors';
+import { useQueryClient } from 'react-query';
+import { useSelector } from 'react-redux';
+import { useFocusEffect } from '@react-navigation/native';
 import {
   CalendarContainer,
   Calendar,
@@ -14,8 +19,46 @@ import {
   Title,
 } from './Calendar.styles';
 
+type IActivitiesQueryProps = {
+  acitivities: Activity[];
+};
+
 const CalendarScreen = () => {
   const goToAgenda = () => {};
+  const queryClient = useQueryClient();
+  const userID = useSelector(getUserId);
+  const [dataList, setDataList] = React.useState<Activity[]>(undefined);
+
+  const { data: activityList, isLoading } = listActivitiesQuery<IActivitiesQueryProps>(
+    { filter: { activityOwnerId: { eq: userID } } },
+    {
+      refetchOnWindowFocus: false,
+      // @ts-ignore
+      select: (data) => ({
+        bulletins: data?.listActivitys?.items ?? [],
+      }),
+    },
+  );
+
+  React.useEffect(() => {
+    setDataList(activityList?.acitivities.slice(0, 4));
+  }, [activityList]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      queryClient.invalidateQueries('ListBulletins');
+    }, [queryClient]),
+  );
+
+  if (isLoading) {
+    return (
+      <SafeArea>
+        <ScreenContainer>
+          <ActivityIndicator />
+        </ScreenContainer>
+      </SafeArea>
+    );
+  }
 
   return (
     <SafeAreaView>
@@ -27,36 +70,18 @@ const CalendarScreen = () => {
         <UpcommingPlansContainer>
           <Title>Upcoming Plans</Title>
           <Spacing size={16} />
-          <PlanContainer>
-            <Plan onPress={goToAgenda}>Workout with Gabby</Plan>
-            <DefaultIcon
-              color="gray4"
-              iconFamily="MaterialIcons"
-              name="keyboard-arrow-right"
-              size={24}
-              onPress={goToAgenda}
-            />
-          </PlanContainer>
-          <PlanContainer>
-            <Plan onPress={goToAgenda}>Yoga with Molly</Plan>
-            <DefaultIcon
-              color="gray4"
-              iconFamily="MaterialIcons"
-              name="keyboard-arrow-right"
-              size={24}
-              onPress={goToAgenda}
-            />
-          </PlanContainer>
-          <PlanContainer>
-            <Plan onPress={goToAgenda}>Train with Hunter</Plan>
-            <DefaultIcon
-              color="gray4"
-              iconFamily="MaterialIcons"
-              name="keyboard-arrow-right"
-              size={24}
-              onPress={goToAgenda}
-            />
-          </PlanContainer>
+          {dataList?.map((activity) => (
+            <PlanContainer>
+              <Plan onPress={goToAgenda}>{activity.title}</Plan>
+              <DefaultIcon
+                color="gray4"
+                iconFamily="MaterialIcons"
+                name="keyboard-arrow-right"
+                size={24}
+                onPress={goToAgenda}
+              />
+            </PlanContainer>
+          ))}
         </UpcommingPlansContainer>
       </ScreenContainer>
     </SafeAreaView>
